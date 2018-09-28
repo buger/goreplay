a792c546d6d2cfa91b318994177b6de45b2808e8
bruce34 <bruce34sw@gmail.com>
2018-09-28 15:56:37 +0100

    Add adjustable output-http-stats-ms

--- output_http.go
+++ output_http.go
@@ -21,8 +21,9 @@ type response struct {
 type HTTPOutputConfig struct {
 	redirectLimit int
 
-	stats   bool
-	workers int
+	stats       bool
+	stats_ms    int
+	workers     int
 
 	elasticSearch string
 
@@ -68,7 +69,7 @@ func NewHTTPOutput(address string, config *HTTPOutputConfig) io.Writer {
 	o.config = config
 
 	if o.config.stats {
-		o.queueStats = NewGorStat("output_http")
+		o.queueStats = NewGorStat("output_http", o.config.stats_ms)
 	}
 
 	o.queue = make(chan []byte, 1000)
@@ -164,8 +165,8 @@ func (o *HTTPOutput) Write(data []byte) (n int, err error) {
 
 		if len(o.queue) > int(workersCount) {
 			o.needWorker <- len(o.queue)
+			}
 		}
-	}
 
 	return len(data), nil
 }
