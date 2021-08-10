3b373695a40624fe5fb7fe56f44d6be232c65742
dimagolomozy <dima.golomozy@gmail.com>
2021-08-10 11:23:46 +0300

    only capture nic with ips

--- capture/capture.go
+++ capture/capture.go
@@ -571,6 +571,15 @@ func (l *Listener) setInterfaces() (err error) {
 	}
 
 	for _, pi := range pifis {
+		if len(pi.Addresses) == 0 {
+			continue
+		}
+
+		if isDevice(l.host, pi) {
+			l.Interfaces = []pcap.Interface{pi}
+			return
+		}
+
 		var ni net.Interface
 		for _, i := range ifis {
 			addrs, _ := i.Addrs()
@@ -582,20 +591,14 @@ func (l *Listener) setInterfaces() (err error) {
 					}
 				}
 			}
-
-			if len(addrs) == 0 && i.Name == pi.Name {
-				ni = i
-				break
-			}
 		}
 
 		if ni.Flags&net.FlagLoopback != 0 {
 			l.loopIndex = ni.Index
 		}
 
-		if isDevice(l.host, pi) {
-			l.Interfaces = []pcap.Interface{pi}
-			return
+		if ni.Flags&net.FlagUp == 0 {
+			continue
 		}
 
 		l.Interfaces = append(l.Interfaces, pi)
